dc9dd53c12b182baca1e4f98566554681eef1323
Anirudh RV <anirudhrv@Raghavans-MacBook-Air-542.local>
2020-03-10 13:31:04 +0530

    initialising XP-3

---
+++ Client/src/SPA_components/CustomRouting.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './App.css';
+import styles from './mystyle.module.css'
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import UploadMultipleFiles from './UploadMultipleFiles';
+import EditPage from './EditPage';
+import Home from './Home';
+import DownloadVideoComponent from './DownloadVideoComponent';
+
+class CustomRouting extends Component {
+//TODO : ADD Footer information
+
+  render() {
+    // if logged in, redirect back to edit page or go to home
+    try{
+    if(this.props.location.state.checkval=== "Yes"){
+      return (
+         <BrowserRouter>
+          <div>
+            <Switch />
+              <Switch>
+              <Route path="/upload" component={UploadMultipleFiles}/>
+              <Route path="/EditPage" component={EditPage}/>
+              <Route path="/DownloadVideoComponent" component={DownloadVideoComponent}/>
+
+              <Redirect to={{
+            pathname: '/upload',
+            state: {userName:this.props.location.state.usercredentials}
+        }}
+/>
+             </Switch>
+          </div>
+        </BrowserRouter>
+      );
+    }
+    else{
+      console.log("failure")
+      return (
+         <div>
+              <Redirect to="/" />
+         </div>
+      );
+    }
+  }
+  catch(e){
+    console.log("catch")
+      return (
+         <div>
+              <Redirect to="/" />
+         </div>
+      );
+    }
+  }
+}
+export default CustomRouting;
